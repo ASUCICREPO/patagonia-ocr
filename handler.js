7dfbe059cf2e0be971c333c648cb2225a38c6618
Andres Bolocco <agbb.cg@gmail.com>
2020-05-21 18:21:54 -0300

    acomodating

--- handler.js
+++ handler.js
@@ -1,62 +1,47 @@
-'use strict';
 /*
 
 https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Textract.html
 https://docs.aws.amazon.com/textract/latest/dg/api-async.html
 */
+const uniqid = require('uniqid');
 
 const authorize = require('./lib/authorizer');
-const uploadS3 = require('./lib/s3Uploader');
+const upload = require('./lib/uploader');
 const callTextract = require('./lib/textractCaller');
 const processBill = require('./lib/billProcessor');
+const respond = require('./lib/responder');
 
-module.exports.process = async (event, context) => {
-
+module.exports.process = async (event) => {
   try {
+    const proceed = authorize(event);
 
-    const proceed = await authorize(event);
-
-    if(!proceed) {
+    if (!proceed) {
       return respond(401, 'Unauthorized');
     }
 
-    const bill = await uploadS3(event);
+    const requestID = `${new Date().getTime()}_${uniqid()}`;
+    const bill = await upload(event, requestID);
 
-    if(!bill) {
-      return respond(502, 'Can\' upload file');
+    if (!bill) {
+      return respond(415, 'Unsupported Media Type');
     }
 
-  ///////
-  return;
-    // TEMP hardcoded object key
-    //const bill = 's3://asu-cic-textract-api-dev-bills/tep_mock_bill_with_notations.pdf';
+    const ocr = await callTextract(bill, requestID);
+    console.log(ocr);
 
+    if (!ocr) {
+      return respond(422, 'Unprocessable Entity');
+    }
 
-    const ocr = await callTextract(bill);
+    const processed = await processBill(ocr);
 
-    if(!ocr) {
-      return respond(502, 'Can\' process file');
+    if (!processed) {
+      return respond(502, 'Problem processing bill after OCR');
     }
 
     return respond(200, ocr);
-
   } catch (e) {
     console.log(e);
     return respond(500, 'Server Error');
   }
-
 };
-
-const respond = (statusCode, data, headers = {}) => {
-
-  return ({
-    statusCode,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify(data)
-  });
-
-};
-
